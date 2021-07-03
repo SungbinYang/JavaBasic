ab50a3c4e7b702a5d4bf74dc8d52054d40205c22
Robert.Yang <ysb05222@naver.com>
2021-07-04 06:02:58 +0900

    변수
    변수

---
+++ src/me/sungbin/chapter1/ch01_05/VariableTest.java
@@ -0,0 +1,12 @@
+package me.sungbin.chapter1.ch01_05;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        int age, count;
+        age = 10;
+
+        int level = 10000;
+        System.out.println(age);
+        System.out.println(level);
+    }
+}
