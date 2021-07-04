547f68ed67c1da0aded766212aba3b9b60533bb4
Robert.Yang <ysb05222@naver.com>
2021-07-04 17:39:21 +0900

    조건문 - if
    조건문 - if

---
+++ src/me/sungbin/chapter1/ch01_14/IfElseTest.java
@@ -0,0 +1,15 @@
+package me.sungbin.chapter1.ch01_14;
+
+import java.util.Scanner;
+
+public class IfElseTest {
+    public static void main(String[] args) {
+
+        System.out.println("나이를 입력해주세요: ");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+
+        if (age >= 8) System.out.println("학교에 다닙니다.");
+        else System.out.println("학교에 다니지 않습니다.");
+    }
+}
