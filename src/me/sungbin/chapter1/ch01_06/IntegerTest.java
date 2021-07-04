1fdfe59ebd1fd2c178e7477d45b6696463b3ae6f
Robert.Yang <ysb05222@naver.com>
2021-07-04 11:18:42 +0900

    자료형 - 정수
    자료형 - 정수

---
+++ src/me/sungbin/chapter1/ch01_06/IntegerTest.java
@@ -0,0 +1,13 @@
+package me.sungbin.chapter1.ch01_06;
+
+public class IntegerTest {
+    public static void main(String[] args) {
+
+        byte bNum = -128;
+        System.out.println(bNum);
+
+//        int num = 12345678900;
+        long lNum = 12345678900L;
+        System.out.println(lNum);
+    }
+}
