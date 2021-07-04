b3e8e658c3f4784ff7236b2eb6c9f30e0d19b557
Robert.Yang <ysb05222@naver.com>
2021-07-04 19:44:55 +0900

    반복문 - while
    반복문 - while

---
+++ src/me/sungbin/chapter1/ch01_17/WhileTest.java
@@ -0,0 +1,16 @@
+package me.sungbin.chapter1.ch01_17;
+
+public class WhileTest {
+    public static void main(String[] args) {
+
+        int num = 1;
+        int sum = 0;
+
+        while(num <= 10) {
+            sum+=num;
+            num++;
+        }
+        System.out.println(sum);
+        System.out.println(num);
+    }
+}
