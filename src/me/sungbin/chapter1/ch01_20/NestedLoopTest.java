dd0f64a49b5164b8b55d0e9f9b6c75dd2b3b5ed8
Robert.Yang <ysb05222@naver.com>
2021-07-04 21:02:37 +0900

    중첩 반복문
    중첩 반복문

---
+++ src/me/sungbin/chapter1/ch01_20/NestedLoopTest.java
@@ -0,0 +1,28 @@
+package me.sungbin.chapter1.ch01_20;
+
+public class NestedLoopTest {
+    public static void main(String[] args) {
+
+        int dan;
+        int count;
+
+        for (dan = 2; dan <= 9; dan++) {
+            for (count = 1; count <= 9; count++) {
+                System.out.println(dan + "x" + count + "=" + dan*count);
+            }
+            System.out.println();
+        }
+
+        dan = 2;
+        count = 1;
+        while (dan <= 9) {
+            count = 1;
+            while (count <= 9) {
+                System.out.println(dan + "x" + count + "=" + dan*count);
+                count++;
+            }
+            dan++;
+            System.out.println();
+        }
+    }
+}
