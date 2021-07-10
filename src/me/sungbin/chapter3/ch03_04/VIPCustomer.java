41beb6a0cd53ebdee6d699fb1ec35072c6c6792d
Robert.Yang <ysb05222@naver.com>
2021-07-10 22:13:50 +0900

    오버라이딩
    오버라이딩

---
+++ src/me/sungbin/chapter3/ch03_04/VIPCustomer.java
@@ -0,0 +1,35 @@
+package me.sungbin.chapter3.ch03_04;
+
+public class VIPCustomer extends Customer {
+
+    double salesRatio;
+    private String agentId;
+
+//    public VIPCustomer() {
+//        super(0, "no-name");
+//        bonusRatio = 0.05;
+//        salesRatio = 0.1;
+//        customerGrade = "VIP";
+//
+//        System.out.println("VIPCustomer() call");
+//    }
+
+    public VIPCustomer(int customerId, String customerName) {
+        super(customerId, customerName);
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+        customerGrade = "VIP";
+        System.out.println("VIPCustomer(int, String) call");
+    }
+
+    public String getAgentId() {
+        return agentId;
+    }
+
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        price -= (int) price * salesRatio;
+        return price;
+    }
+}
