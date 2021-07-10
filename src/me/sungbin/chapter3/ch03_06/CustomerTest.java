6d8bc791f0c73cb1ad5b03272ec31a2da83f9c77
Robert.Yang <ysb05222@naver.com>
2021-07-11 04:32:20 +0900

    다운캐스팅과 instanceof
    다운캐스팅과 instanceof

--- src/me/sungbin/chapter3/ch03_06/CustomerTest.java
+++ src/me/sungbin/chapter3/ch03_06/CustomerTest.java
@@ -14,18 +14,23 @@ public class CustomerTest {
         Customer customerP = new GoldCustomer(10040, "Percy");
         Customer customerK = new VIPCustomer(10050, "Kim");
 
-        customerList.add(customerT);
-        customerList.add(customerJ);
-        customerList.add(customerE);
-        customerList.add(customerP);
-        customerList.add(customerK);
+//        customerList.add(customerT);
+//        customerList.add(customerJ);
+//        customerList.add(customerE);
+//        customerList.add(customerP);
+//        customerList.add(customerK);
+//
+//        for (Customer customer : customerList) {
+//            int cost = customer.calcPrice(price);
+//            System.out.println(customer.getCustomerName() + "님이" + cost + "원 지불하셨습니다.");
+//            System.out.println(customer.getCustomerName() + "님의 현재 보너스 포인트는 " + customer.bonusPoint + "입니다.");
+//            System.out.println(customer.showCustomerInfo());
+//            System.out.println("================================");
+//        }
 
-        for (Customer customer : customerList) {
-            int cost = customer.calcPrice(price);
-            System.out.println(customer.getCustomerName() + "님이" + cost + "원 지불하셨습니다.");
-            System.out.println(customer.getCustomerName() + "님의 현재 보너스 포인트는 " + customer.bonusPoint + "입니다.");
-            System.out.println(customer.showCustomerInfo());
-            System.out.println("================================");
+        if (customerE instanceof GoldCustomer) {
+            GoldCustomer vc = (GoldCustomer) customerE;
+            System.out.println(customerE.showCustomerInfo());
         }
     }
 }
