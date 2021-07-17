13f5de762de198c69b196b0e3bcce90d835ce81f
Robert.Yang <ysb05222@naver.com>
2021-07-17 11:46:52 +0900

    Queue 구현하기
    Queue 구현하기

---
+++ src/me/sungbin/chapter5/ch05_05/MyListQueueTest.java
@@ -0,0 +1,19 @@
+package me.sungbin.chapter5.ch05_05;
+
+public class MyListQueueTest {
+    public static void main(String[] args) {
+
+        MyLinkedQueue listQueue = new MyLinkedQueue();
+
+        listQueue.enQueue("A");
+        listQueue.enQueue("B");
+        listQueue.enQueue("C");
+
+        listQueue.printQueue();
+
+        System.out.println(listQueue.deQueue());
+        System.out.println(listQueue.deQueue());
+
+        listQueue.printQueue();
+    }
+}
