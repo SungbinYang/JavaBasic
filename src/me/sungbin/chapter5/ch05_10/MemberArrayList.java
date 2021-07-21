74cb1dcd1f0af1d2151fb3cbf1bfe7aff875ee04
Robert.Yang <ysb05222@naver.com>
2021-07-21 21:37:46 +0900

    Collection 요소를 순회하는 Iterator
    Collection 요소를 순회하는 Iterator

--- src/me/sungbin/chapter5/ch05_10/MemberArrayList.java
+++ src/me/sungbin/chapter5/ch05_10/MemberArrayList.java
@@ -1,6 +1,7 @@
 package me.sungbin.chapter5.ch05_10;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MemberArrayList {
 
@@ -19,13 +20,23 @@ public class MemberArrayList {
     }
 
     public boolean removeMember(int memberId) {
-        for (int i = 0; i < arrayList.size(); i++) {
-            Member member = arrayList.get(i);
+//        for (int i = 0; i < arrayList.size(); i++) {
+//            Member member = arrayList.get(i);
+//
+//            int tempId = member.getMemberId();
+//            if (tempId == memberId) {
+//                arrayList.remove(i);
+//                return true;
+//            }
+//        }
+        Iterator<Member> ir = arrayList.iterator();
 
+        while (ir.hasNext()) {
+            Member member = ir.next();
             int tempId = member.getMemberId();
-            if (tempId == memberId) {
-                arrayList.remove(i);
-                return true;
+            if(tempId == memberId){            // 멤버아이디가 매개변수와 일치하면
+                arrayList.remove(member);           // 해당 멤버를 삭제
+                return true;                   // true 반환
             }
         }
         System.out.println(memberId + "가 존재하지 않습니다.");
