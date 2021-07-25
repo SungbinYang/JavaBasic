fbe8d1cbaa5ad97cac6f6e3c9c1ddadf7ba62761
Robert.Yang <ysb05222@naver.com>
2021-07-25 10:36:54 +0900

    표준 입출력 스트림
    표준 입출력 스트림

---
+++ src/me/sungbin/chapter6/ch06_13/SystemInTest1.java
@@ -0,0 +1,20 @@
+package me.sungbin.chapter6.ch06_13;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SystemInTest1 {
+    public static void main(String[] args) {
+
+        System.out.println("알파벳 하나를 쓰고 [Enter]를 누르세요");
+
+        int i;
+        try (InputStreamReader irs = new InputStreamReader(System.in)) {
+            while ((i = irs.read()) != '\n') {
+                System.out.print((char) i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
