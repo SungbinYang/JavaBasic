2ec417b1447c2e94ff9bf40c5d3d33be80910842
Robert.Yang <ysb05222@naver.com>
2021-08-01 09:38:14 +0900

    미로찾기
    미로찾기

---
+++ src/me/sungbin/chapter8/ch08_06/Move.java
@@ -0,0 +1,14 @@
+package me.sungbin.chapter8.ch08_06;
+
+public class Move {
+
+    int direction = 0;
+
+    public int x = 0;
+    public int y = 0;
+
+    public Move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
